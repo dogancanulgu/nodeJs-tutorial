388abccd426cbf82d137b584b723b611c7ee3a9b
Dogancan Ulgu <dogancanulgu@gmail.com>
2021-10-29 01:02:21 +0300

    node Http was studied

---
+++ nodeHttp/index.js
@@ -0,0 +1,29 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>INDEX SAYFASI</h1>");
+  } else if (url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("ABOUT SAYFASI");
+  } else if (url === "/contact") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("CONTACT SAYFASI");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write("<h1>404 SAYFA BULUNAMADI</h1>");
+  }
+  //   console.log(url);
+  //   console.log("Bir istek gönderildi");
+  //   res.write("MERHABA");
+  res.end();
+});
+
+const port = 3000;
+
+server.listen(port, () => {
+  console.log(`Sunucu port ${port} de başlatıldı`);
+});
